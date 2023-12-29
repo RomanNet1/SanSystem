3a4742e78afee07cbc4b543a4907c3755e5fd434
RomanNet1 <ramulrimov004@gmail.com>
2023-12-29 13:40:52 +0300

    Номая планета И создание конструктора для планет

--- src/Panel.java
+++ src/Panel.java
@@ -2,21 +2,29 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Panel extends JPanel {
-    Planet earth;
-    Planet venus;
+
+    Planet earth; // планета Земля
+    Planet venus;// планета Венера
+    Planet mercure;
+    static Planet base; // базовая планета, от которой всё рисуется
+
+
+    /*
+------------------------------------------------------------------------------------//
+------     Создание планет на панели    --------------------------------------------//
+------------------------------------------------------------------------------------//
+*/
+
+
     public Panel(){
 
-        earth = new Planet();
-        earth.c = Color.blue;  // установка цвета планеты
-        earth.dist = 149598261;
-        earth.radius = 6371;
-        earth.days = 365.256363004;
+        earth = new Planet( Color.blue,149598261,6371, 365.256363004);
 
-        venus = new Planet();
-        venus.c = Color.orange;  // установка цвета планеты
-        venus.dist = 108208930;
-        venus.radius = 6051;
-        venus.days = 243.023;
+        venus = new Planet( Color.orange,  108208930, 6051, 243.023);
+
+        mercure = new Planet( Color.red,  57909227, 2439, 87.969);
+
+        base = earth;
 
     }
     @Override
@@ -26,5 +34,13 @@ public class Panel extends JPanel {
         g.fillOval(200,200,200,200);
         earth.draw(g); // g -  холст и кисти
         venus.draw(g); // g -  холст и кисти
+        mercure.draw(g);
     }
 }
+
+//Было
+
+//        earth.c = Color.blue;  // установка цвета планеты
+//        earth.dist = 149598261;
+//        earth.radius = 6371;
+//        earth.days = 365.256363004;
--- src/Planet.java
+++ src/Planet.java
@@ -11,17 +11,26 @@ public class Planet { // класс планеты
     int X0 = 300;
     int Y0 = 300;
 
-    int dist; // растояние до солнца в км
-    int radius; // размер панеты в км
-    double days; // Год планеты в земных днях
+    private int dist; // растояние до солнца в км
+    private int radius; // размер панеты в км
+    private double days; // Год планеты в земных днях
 
-    public Planet() {
+
+//------------------------------------------------//
+//------     Конструктор создания планет  --------//
+//------------------------------------------------//
+
+    public Planet(Color c, int d, int r, double days) {
+        this.c = c;
+        this.dist = d;
+        this.radius = r;
+        this.days = days;
         //выполнить один раз
         Timer t;
         t = new Timer(50, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double speed = 365.256363004 / days  * 2;
+                double speed = Panel.base.days / days  * 2;
 
                 ALPHA+=speed;
                 //переменная p глобальная
@@ -32,20 +41,25 @@ public class Planet { // класс планеты
         t.start();
     }
 
+
+//--------------------------------------------------------//
+//------     Рисование планет и эклиптики          -------//
+//--------------------------------------------------------//
+
     public void draw(Graphics g) {
 
         // 1 - эклиптика
 
         g.setColor(c);
-        double z = 149598261;
+        double z = Panel.base.dist;
         R = (int) (dist / z * 300);
         int xx = (300 - R);
         g.drawOval(xx, xx, R * 2, R * 2);
-        //System.out.println(R);
+
 
         // 2 - сама планета
 
-        z = 6371;
+        z = Panel.base.radius;
         W = (int) (radius / z * 60);
         int x = (int) (X0 + R * Math.cos(Math.toRadians(ALPHA) ));
         int y = (int) (Y0 + R * Math.sin(Math.toRadians(ALPHA)));
